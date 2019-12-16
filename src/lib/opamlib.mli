82c67c9444318942316dc5e72b083fc295151492
Raja Boujbel <raja.boujbel@ocamlpro.com>
2019-12-16 17:17:55 +0100

    opamlib: extra arg to pass other arguments

--- src/lib/opamlib.mli
+++ src/lib/opamlib.mli
@@ -39,41 +39,47 @@ val var: dirname -> string -> string
 
 val init: dirname -> repository_name -> url -> unit
 
-val install: dirname -> ?version:version -> name -> unit
+val install: dirname -> ?version:version -> ?oargs:string -> name -> unit
 
-val install_code: dirname -> ?version:version -> name -> int
+val install_code: dirname -> ?version:version -> ?oargs:string -> name -> int
 
-val install_dir: dirname -> ?recs:bool -> ?subpath:string -> dirname -> unit
+val install_dir:
+  dirname -> ?recs:bool -> ?subpath:string -> ?oargs:string -> dirname -> unit
 
-val reinstall: dirname -> ?version:version -> name -> unit
+val reinstall: dirname -> ?version:version -> ?oargs:string -> name -> unit
 
-val remove: dirname -> ?auto:bool -> name -> unit
+val remove: dirname -> ?auto:bool -> ?oargs:string -> name -> unit
 
 val remove_dir:
-  dirname -> ?auto:bool -> ?recs:bool -> ?subpath:string -> dirname -> unit
+  dirname -> ?auto:bool -> ?recs:bool -> ?subpath:string -> ?oargs:string ->
+  dirname -> unit
 
-val update: dirname -> unit
+val update: ?oargs:string -> dirname -> unit
 
-val upgrade: dirname -> ?fake:bool -> name list -> unit
+val upgrade: dirname -> ?fake:bool -> ?oargs:string -> name list -> unit
 
 val pin:
-  dirname -> ?recs:bool -> ?subpath:string -> ?action:bool
-  -> name -> dirname -> unit
+  dirname -> ?recs:bool -> ?subpath:string -> ?action:bool -> ?oargs:string ->
+  name -> dirname -> unit
 
-val vpin: dirname -> name -> version -> unit
+val vpin: dirname -> ?oargs:string -> name -> version -> unit
 
-val pin_kind: dirname -> ?action:bool -> ?kind:string -> name -> string -> unit
+val pin_kind:
+  dirname -> ?action:bool -> ?kind:string -> ?oargs:string ->
+  name -> string -> unit
 
 val pin_dir:
-  dirname -> ?recs:bool -> ?subpath:string -> ?action:bool -> ?kind:string
-  -> dirname -> unit
+  dirname -> ?recs:bool -> ?subpath:string -> ?action:bool -> ?kind:string ->
+  ?oargs:string -> dirname -> unit
 
-val unpin: dirname -> ?action:bool -> name -> unit
+val unpin: dirname -> ?action:bool -> ?oargs:string -> name -> unit
 
 val unpin_dir:
-  dirname -> ?recs:bool -> ?subpath:string -> ?action:bool -> dirname -> unit
+  dirname -> ?recs:bool -> ?subpath:string -> ?action:bool -> ?oargs:string ->
+  dirname -> unit
 
-val pin_edit: dirname -> ?action:bool -> name -> (filename -> unit) -> unit
+val pin_edit:
+  dirname -> ?action:bool -> ?oargs:string -> name -> (filename -> unit) -> unit
 
 val pinned: dirname -> string list list
 
